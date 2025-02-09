a879ad7b7a49fd350e8e4dd14642330715e40141
bilgecerenyilmaz <138780424+bilgecerenyilmaz@users.noreply.github.com>
2025-02-09 23:40:06 +0300

    Add files via upload
    Var olan .dat dosyasının içeriğini okumak için yazılmış bir kod.

---
+++ Ogrenciler.dat Okuma.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+struct Student {
+	int id;
+	char name[16];
+	char surname[16];
+	int average;
+};
+int main() {
+	FILE* file = fopen("C:\\Users\\Ceren\\Downloads\\Ogrenciler.dat", "rb");
+	struct Student student;
+	while (fread(&student, sizeof(struct Student), 1, file) == 1) {
+		printf(" -------------------------------------------\n");
+		printf(" ID       : %d\n", student.id);
+		printf(" Name     : %s\n", student.name);
+		printf(" Surname  : %s\n", student.surname);
+		printf(" Average  : %d\n", student.average);
+		printf(" -------------------------------------------\n\n");
+	}
+	fclose(file);
+	return 0;
+}
+
